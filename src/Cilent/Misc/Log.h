c453e6163f68b2787919ce0a42a13ac204766d5f
Retro Swan <swan@silenttowergames.com>
2022-09-22 02:14:48 -0400

    Logging to file

--- src/Cilent/Misc/Log.h
+++ src/Cilent/Misc/Log.h
@@ -9,5 +9,7 @@ typedef enum Cilent_Log_Type
 
 void Cilent_Log(Cilent_Log_Type type, char* fmt, ...);
 
+void Cilent_Log_ToFile(char value);
+
 #define debug_log_type(type, ...) Cilent_Log(CILENT_LOG_##type, __VA_ARGS__)
 #define debug_log(...) debug_log_type(INFO, __VA_ARGS__)
